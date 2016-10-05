1126d2e7838cb8774199a83a20209f21284bd0b2
zqz <zheng.q@husky.neu.edu>
2016-10-05 12:16:38 -0400

    TopKFrequentElements

--- src/PocketGemsMianjing/Strstr.java
+++ src/PocketGemsMianjing/Strstr.java
@@ -6,5 +6,19 @@ package PocketGemsMianjing;
  解法，直接暴⼒力法，最坏情况O(mn)，⽐比如⼀一个
  是"aaaaaaaaaaaaaaaaaaaaab"，⼀一个是"ab"
  */
-public class Strstr {
+public class Strstr {//brute force
+    public int strStr(String haystack,String needle){
+        int l1=haystack.length();
+        int l2=needle.length();
+        if(l1<l2) return -1;
+        if(l2==0||needle==null) return 0;
+        int difference=l1-l2;
+        for(int i=0;i<=difference;i++){
+            if(haystack.substring(i,i+l2).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
---
+++ src/PocketGemsMianjing/TopKFrequentElements.java
@@ -0,0 +1,55 @@
+package PocketGemsMianjing;
+import java.util.*;
+/**
+ * Created by zqz on 2016/10/5.
+ */
+public class TopKFrequentElements {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> freq=new HashMap<>();
+        List<Integer>[] bucket=new List[nums.length+1];
+        List<Integer> res=new ArrayList<>();
+        for(int num:nums){//store freq
+            freq.put(num,freq.getOrDefault(num,0)+1);
+        }
+        for(int key:freq.keySet()){//就是把它反向存array里
+            int frequency=freq.get(key);
+            if(bucket[frequency]==null){
+                bucket[frequency]=new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+        for(int pos=bucket.length-1;pos>0;pos--){
+            if(bucket[pos]!=null){
+                for(int i=0;i<bucket[pos].size()&&res.size()<k;i++){
+                    res.add(bucket[pos].get(i));
+                }
+            }
+        }
+        return res;
+    }
+    public List<Integer> topKFrequentDetailComment(int[] nums, int k) {
+        Map<Integer,Integer> freq=new HashMap<>();
+
+        List<Integer> res=new ArrayList<>();
+        for(int num:nums){//store freq
+            freq.put(num,freq.getOrDefault(num,0)+1);
+        }
+
+        List<Integer>[] array=new List[nums.length+1];//the smallest number is 1,so 0+1=1
+        for(int key:freq.keySet()){
+            int frequency=freq.get(key);
+            if(array[frequency]==null){//fre quent here
+                array[frequency]=new ArrayList<>();
+            }
+            array[frequency].add(key);//add all key to this certain frequent
+        }
+        for(int pos=array.length-1;pos>0;pos--){//get from large to small
+            if(array[pos]!=null){
+                for(int i=0;i<array[pos].size()&&res.size()<k;i++){//first check over flow, then check get k already
+                    res.add(array[pos].get(i));
+                }
+            }
+        }
+        return res;
+    }
+}
