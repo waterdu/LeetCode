80ea3fe66700661cfebf61c5476f8b35970e2166
zqz <zheng.q@husky.neu.edu>
2016-10-15 12:56:08 -0400

    LongestPalindromeSubstring

--- src/FacebookDianmian/LongestPalindromeSubstring.java
+++ src/FacebookDianmian/LongestPalindromeSubstring.java
@@ -4,5 +4,26 @@ package FacebookDianmian;
  * Created by zqz on 2016/10/8.
  */
 public class LongestPalindromeSubstring {
-
+    private String longest="";
+    public String longestPalindrome(String s) {
+        //spread from the center
+        //时间 O(n^2) 空间 O(1)
+        for(int i=0;i<s.length();i++){
+            helper(s,i,0);//odd
+            helper(s,i,1);//even
+        }
+        return longest;
+    }
+    private void helper(String s,int index,int offset){
+        int left=index;
+        int right=index+offset;
+        while(left>=0&& right<s.length() &&s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+        }
+        String currPalindrome=s.substring(left+1,right-1+1);
+        if(longest.length()>currPalindrome.length()){
+            longest=currPalindrome;
+        }
+    }
 }
---
+++ src/FacebookDianmian/stringComparator.java
@@ -0,0 +1,22 @@
+package FacebookDianmian;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Created by zqz on 2016/10/15.
+ * 出了道题：自然string comparator。不知道的搜下natrual string comparision。
+ * 就是string 比较的时候考虑里面数字的大小，比如 abc9 < abc123,abc > ab9  因为char比digit重要。进入onsite了，求bless。求加分！
+ 要的就是这样：
+ */
+public class stringComparator {
+//    public static void sort(String[] s){
+//        Arrays.sort(a, new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                // provide your comparison logic here
+//                return 0;
+//            }
+//        });
+//    }
+}
